28ab9eddcf2b47afa0ec93b80189f91fc2061294
Gedearya <igd.ad.pratama@gmail.com>
2026-02-11 09:06:54 +0700

    feat: add exercise for practicing javascript

---
+++ assets/scripts/exercise.js
@@ -0,0 +1,63 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Gede Arya",
+    phone: "+6285-891-234-889",
+    email: "gedearya@gmail.com",
+    address: "Jakarta, Indonesia",
+  },
+  {
+    id: 2,
+    name: "Ardanu Wicaksono",
+    phone: "+6281-234-567-890",
+    email: "ardanu@gmail.com",
+    address: "Riau, Indonesia",
+  },
+  {
+    id: 3,
+    name: "Muhammad Alroy",
+    phone: "+6283-456-789-012",
+    email: "alroy@gmail.com",
+    address: "Surabaya, Indonesia",
+  },
+  {
+    id: 4,
+    name: "Dimas Aditya",
+    phone: "+6284-567-890-123",
+    email: "dimas@gmail.com",
+    address: "Bandung, Indonesia",
+  },
+  {
+    id: 5,
+    name: "Lazuardy Anugrah",
+    phone: "+6285-678-901-234",
+    email: "lazuardy@mail.com",
+    address: "Tangerang, Indonesia",
+  },
+];
+
+console.log("contacts:", contacts);
+console.table(contacts);
+console.log(contacts[0]);
+
+let firstName = "I Gede Arya";
+let lastName = "Danny Pratama";
+let phone = "+6285-891-234-889";
+let email = "gedearya@gmail.com";
+let address = "Jakarta, Indonesia";
+let hobby = ["Reading", "Traveling", "Coding"];
+
+console.log("Full Name:", firstName + " " + lastName);
+console.log("Phone:", phone);
+console.log("Email:", email);
+console.log("Address:", address);
+console.log("Hobby:", hobby);
+
+const a = 25;
+const b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
